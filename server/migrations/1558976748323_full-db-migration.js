d4a9020ad31566d6a28cee160e82c82502b3ce8d
Kayahs <akshaykmanchanda@gmail.com>
2019-05-28 13:04:19 -0400

    Set up migrations for users, quizzes, categories, questions

---
+++ server/migrations/1558976748323_full-db-migration.js
@@ -0,0 +1,36 @@
+exports.up = pgm => {
+  //1. Users Table
+  pgm.sql(`
+    CREATE TABLE "jeopardy"."users" (
+      "id" SERIAL PRIMARY KEY,
+      "email" TEXT UNIQUE NOT NULL,
+      "password" TEXT NOT NULL
+    );
+  `),
+    //2. Quizzes Table
+    pgm.sql(`
+    CREATE TABLE "jeopardy"."quizzes" (
+      "id" SERIAL PRIMARY KEY,
+      "title" TEXT NOT NULL,
+      "owner_id" INTEGER REFERENCES users(id)
+    );
+  `),
+    //3. Categories Table
+    pgm.sql(`
+    CREATE TABLE "jeopardy"."categories" (
+      "id" SERIAL PRIMARY KEY,
+      "name" TEXT NOT NULL,
+      "quiz_id" INTEGER REFERENCES quizzes(id) NOT NULL
+    );
+  `),
+    //5. Questions Table
+    pgm.sql(`
+      CREATE TABLE "jeopardy"."questions" (
+        "id" SERIAL PRIMARY KEY,
+        "category_id" INTEGER REFERENCES categories(id) NOT NULL,
+        "quiz_id" INTEGER REFERENCES quizzes(id) NOT NULL,
+        "question" TEXT NOT NULL,
+        "answer" TEXT NOT NULL
+      );
+  `)
+}
