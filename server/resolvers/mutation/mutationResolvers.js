c19286dfb8fb4f652198cac0dd55e047594aba87
Kayahs <akshaykmanchanda@gmail.com>
2019-06-21 15:57:17 -0400

    Added login mutation

--- server/resolvers/mutation/mutationResolvers.js
+++ server/resolvers/mutation/mutationResolvers.js
@@ -26,6 +26,47 @@ module.exports = {
       return {
         user: newUserResult.rows[0]
       }
+    },
+    async login(
+      parent,
+      {
+        input: { email, password }
+      },
+      { app, req, postgres },
+      info
+    ) {
+      const emailLowerCase = email.toString().toLowerCase()
+
+      const getUser = {
+        text: "SELECT * FROM jeopardy.users WHERE email=$1",
+        values: [emailLowerCase]
+      }
+
+      const getUserResult = await postgres.query(getUser)
+      const user = getUserResult.rows[0]
+      if (!user) {
+        return {
+          error: {
+            code: 401,
+            message: "Email or Password is Invalid"
+          }
+        }
+      }
+
+      const validPassword = await bcrypt.compare(password, user.password)
+
+      if (!validPassword) {
+        return {
+          error: {
+            code: 401,
+            message: "Email or Password is Invalid"
+          }
+        }
+      }
+
+      return {
+        user: user
+      }
     }
   }
 }
