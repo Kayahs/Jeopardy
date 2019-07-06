34734653b2b0657e5f91f0be2c6b1303ce03ba2b
Kayahs <akshaykmanchanda@gmail.com>
2019-07-05 22:23:48 -0400

    Added logout button to dashboard and mutation

---
+++ client/src/components/auth/Logout.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { useMutation } from "react-apollo-hooks"
+import { LOGOUT_MUTATION } from "gql/mutations"
+
+const Logout = () => {
+  const logout = useMutation(LOGOUT_MUTATION)
+  return (
+    <div>
+      <button onClick={logout}>Logout</button>
+    </div>
+  )
+}
+
+export default Logout
