85581e9c8e11354e48b5079d2e049140f6a0db20
Kayahs <akshaykmanchanda@gmail.com>
2019-07-06 00:18:50 -0400

    Added signup form and name input

--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,7 +1,59 @@
 import React from "react"
+import * as Yup from "yup"
+import { useMutation } from "react-apollo-hooks"
+import { Link } from "react-router-dom"
+import { Formik } from "formik"
+
+import { SIGNUP_MUTATION } from "gql/mutations"
+import { FormContext } from "lib/contexts"
+import {
+  NameInput,
+  EmailInput,
+  PasswordInput,
+  SubmitButton
+} from "components/form"
 
 const SignUp = () => {
-  return <div>SignUp.</div>
+  const signup = useMutation(SIGNUP_MUTATION)
+  const labels = {
+    fullname: "Please enter your name.",
+    email: "Please enter your email.",
+    password: "Please enter your password"
+  }
+  return (
+    <Formik
+      initialValues={{
+        fullname: "",
+        email: "",
+        password: ""
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        signup({ variables: { input: values } })
+        setSubmitting(false)
+      }}
+      validationSchema={Yup.object().shape({
+        fullname: Yup.string(),
+        email: Yup.string()
+          .email()
+          .required("required"),
+        password: Yup.string().required("required")
+      })}
+    >
+      {props => (
+        <FormContext.Provider value={{ ...props, labels }}>
+          <form onSubmit={props.handleSubmit}>
+            <React.Fragment>
+              <NameInput />
+              <EmailInput />
+              <PasswordInput />
+              <SubmitButton />
+              <Link to="/">Existing Account?</Link>
+            </React.Fragment>
+          </form>
+        </FormContext.Provider>
+      )}
+    </Formik>
+  )
 }
 
 export default SignUp
--- client/src/components/form/index.js
+++ client/src/components/form/index.js
@@ -2,5 +2,6 @@ import EmailInput from "./EmailInput"
 import PasswordInput from "./PasswordInput"
 import ErrorText from "./ErrorText"
 import SubmitButton from "./SubmitButton"
+import NameInput from "./NameInput"
 
-export { EmailInput, PasswordInput, ErrorText, SubmitButton }
+export { EmailInput, PasswordInput, ErrorText, SubmitButton, NameInput }
